5d994899c95c5f5c098d6663760e7b9c50b7aa89
Alexx280 <alexx280@yandex.ru>
2017-09-11 16:20:41 +0700

    Добавлен второй блок

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
-import { AppComponent } from './app.component';
-import { FaceComponent } from './face/face.component';
-import { UpComponent } from './up/up.component';
-import { Block3Component } from './block3/block3.component';
+import {AppComponent} from "./app.component";
+import {FaceComponent} from "./face/face.component";
+import {UpComponent} from "./up/up.component";
+import {Block3Component} from "./block3/block3.component";
+import {Block2Component} from "./block2/name.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     FaceComponent,
     UpComponent,
-    Block3Component
+    Block3Component,
+    Block2Component
   ],
   imports: [
     BrowserModule,
