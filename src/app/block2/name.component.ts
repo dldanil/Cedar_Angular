226106c27faaa756187ea249aeef0ceeee6f78f5
danil <dildanil@mail.ru>
2017-09-12 16:09:05 +0600

    изменения

--- src/app/block2/name.component.ts
+++ src/app/block2/name.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-name',
   templateUrl: './name.component.html',
   styleUrls: ['./name.component.css']
 })
-export class MyComponent implements OnInit {
+export class Block2Component implements OnInit {
 
   constructor() { }
 
